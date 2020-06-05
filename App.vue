650ce75fbc5be83f22d4b316823d6e234946d636
sumiaoqing815 <sumiaoqing815@gmail.com>
2020-06-05 17:43:57 +0800

    uniapp_app

---
+++ App.vue
@@ -0,0 +1,29 @@
+
+<script>
+	export default {
+		onLaunch: function() {
+			uni.getSystemInfo({
+				success:function(e){
+				global.statusBar=e.statusBarHeight;
+				if(e.platform='android')
+				{
+					global.customBar=e.statusBarHeight+50
+				}else
+				{
+					global.customBar=e.statusBarHeight+45
+				}
+				}
+			})
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ pages/login/index.vue
@@ -0,0 +1,233 @@
+<template>
+	<view class="content">
+		<view class="navigation-bar" :style="{height:height+'px'}">
+		</view>
+		
+		<view class="icon-text">
+			<image src="./icon/icon.jpeg" class="app-icon"></image>
+			<view class="text">
+				欢迎登录吉视传媒
+			</view>
+		</view>
+		
+		<view class="login-form">
+			<view class="phone-name">
+				<image src="./icon/手机.png" mode="aspectFit" class="phone-icon"></image>
+				<input type="text" :value="loginForm.username"  class="phone-iput" placeholder="手机号/用户名" @focus="focusColorName"  @blur="blurColorName">
+				<view class="line" :class="isfocusName?'blue':'gray'">
+				</view>
+			</view>
+			<view class="password">
+				<image src="./icon/锁.png"  mode="aspectFit" class="password-icon"></image>
+				<input :type="pwdType"   :value="loginForm.password" class="pwd-iput" placeholder="密码" @focus="focusColorPwd"  @blur="blurColorPwd">
+				<image src="./icon/显示.png"  mode="aspectFit" class="none-block" @click="changeType"></image>
+				<view class="line" :class="isfocusPwd?'blue':'gray'">
+				</view>
+			</view>
+			<view class="forget-password">
+				<text class="forget-pwd-text">忘记密码</text>
+			</view>
+		</view>
+		
+		<view class="login-button__register">
+			<view class="button-login">
+				 登录
+			</view>
+			<view class="login-text">
+				<text class="no-account">还没有账号？</text>
+				<text class="register-now">立即注册</text>
+			</view>	
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				height: 0,
+				loginForm:{
+					username:'',
+					password:''
+				},
+				isfocusName:false,
+				isfocusPwd:false,
+				pwdType:'password'
+			}
+		},
+		onLoad() {
+			this.height = global.customBar
+		},
+		methods: {
+            focusColorName()
+			{
+				this.isfocusName=true;		
+			},
+			blurColorName()
+			{
+			this.isfocusName=false;
+			},
+			focusColorPwd()
+			{
+				this.isfocusPwd=true;		
+			},
+			blurColorPwd()
+			{
+			this.isfocusPwd=false;
+			},
+			changeType()
+			{
+				if(this.pwdType=='password')
+				{
+					this.pwdType='text'
+				}else
+				{
+					this.pwdType='password'
+				}	
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.navigation-bar {
+		width: 100%;
+		background-color: red;
+		position: relative;
+	}
+	.icon-text
+	{
+		width: 100%;
+		height: 360rpx;
+		position: relative;
+	}
+	.app-icon{
+		width: 160rpx;
+		height: 160rpx;
+		border-radius: 20rpx;
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+		top: 60rpx;
+	}
+	.text{
+		transform: translateX(-50%);
+		left: 50%;
+		position: absolute;
+		bottom: 40rpx;
+		font-size: 46rpx;
+		font-weight: 400;
+	}
+	.login-form{
+		width: 100%;
+		height: 360rpx;
+		display: flex;
+		flex-direction: column;
+	}
+	.phone-name{
+		height: 140rpx;
+		width: 100%;
+		position: relative;
+	}
+	.phone-icon{
+		width: 45rpx;
+		height: 45rpx;
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		left: 30rpx;
+	}
+	.phone-iput{
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		left: 100rpx;
+	}
+	.line{
+		position: absolute;
+		bottom: 0rpx;
+		height: 2rpx;
+		width: 670rpx;
+		transform: translateX(-50%);
+		left: 50%;
+	}
+	.password{
+		position: relative;
+		height: 160rpx;
+		width: 100%;
+	}
+	.password-icon{
+		width: 45rpx;
+		height: 45rpx;
+		position: absolute;
+		left: 30rpx;
+		top: 70rpx;
+	}
+	.pwd-iput{
+		position:absolute ;
+		left: 100rpx;
+		top: 70rpx;
+	}
+	.none-block{
+		width: 45rpx;
+		height: 45rpx;
+		position: absolute;
+		right: 40rpx;
+		top: 70rpx;
+	}
+	.forget-password{
+		flex: 1;
+		position: relative;
+	}
+	.forget-pwd-text{
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		right: 40rpx;
+		font-size:26rpx;
+		color: #BFBFBF;
+	}
+	.login-button__register{
+		width: 100%;
+		position: relative;
+	}
+	.button-login{
+		width: 600rpx;
+		height: 100rpx;
+		top: 120rpx;
+		border-radius: 60rpx;
+		position: absolute;
+		transform: translateX(-50%);
+		left: 50%;
+		text-align: center;
+		line-height: 100rpx;
+		background-color: #007AFF;
+		color: #FFFFFF;
+	}
+	.login-text{
+		position: absolute;
+		top: 260rpx;
+		transform: translateX(-50%);
+		left: 50%;
+		font-size: 32rpx;
+		
+	}
+	.no-account{
+		color: #808080;
+	}
+	.register-now{
+		color: #007AFF;
+	}
+	.blue{
+		background-color:#007AFF
+	}
+	.gray{
+		background-color:#BFBFBF
+	}
+</style>
